b2a4937f6d5a2e5b1c493804b765a04a3c3ae667
Brett Saiki <bksaiki@gmail.com>
2020-12-23 15:17:28 -0800

    Update version to 0.1.1

--- src/base.h
+++ src/base.h
@@ -6,7 +6,7 @@
 
 #define MINIM_VERSION_MAJOR     0
 #define MINIM_VERSION_MINOR     1
-#define MINIM_VERSION_PATCH     0
-#define MINIM_VERSION_STR       "0.1.1-dev"
+#define MINIM_VERSION_PATCH     1
+#define MINIM_VERSION_STR       "0.1.1"
 
 #endif
