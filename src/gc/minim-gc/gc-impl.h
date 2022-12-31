68c760c42c37a89fc7e8b61e7cd1fe2880b46a21
Brett Saiki <bksaiki@gmail.com>
2022-12-31 11:30:10 -0800

    missing files

---
+++ src/gc/minim-gc/gc-impl.h
@@ -0,0 +1,59 @@
+#ifndef _MINIM_GC_IMPL_H_
+#define _MINIM_GC_IMPL_H_
+
+#include <limits.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include "gc-types.h"
+
+/* Debug */
+#define GC_DEBUG        0
+
+/* GC Parameters */
+#define GC_MIN_AUTO_COLLECT_SIZE     (8 * 1024 * 1024)
+#define GC_TABLE_LOAD_FACTOR         0.75
+#define GC_MINOR_PER_MAJOR           15
+
+/* Heap heuristic */
+#if defined (_WIN32) || defined (_WIN64)
+#define GC_HEAP_HEURISTIC   0
+#else
+#define GC_HEAP_HEURISTIC   1
+#endif
+
+/************ gc_t interface ************/
+
+gc_t *gc_create(void *stack);
+void gc_destroy(gc_t* gc);
+
+void gc_pause(gc_t *gc);
+void gc_resume(gc_t *gc);
+
+void gc_remove(gc_t *gc, void *ptr, int destroy);
+
+gc_record_t *gc_alloc_record(gc_t *gc, size_t size, gc_dtor_t dtor, gc_mark_t mrk);
+gc_record_t *gc_calloc_record(gc_t *gc, size_t nmem, size_t size, gc_dtor_t dtor, gc_mark_t mrk);
+gc_record_t *gc_realloc_record(gc_t *gc, gc_record_t *r, size_t size, gc_dtor_t dtor, gc_mark_t mrk);
+
+gc_record_t *gc_get_record(gc_t *gc, void *ptr);
+void gc_add_record(gc_t *gc, gc_record_t *record);
+
+void gc_collect(gc_t *gc);
+
+void gc_register_dtor(gc_t *gc, void *ptr, gc_dtor_t dtor);
+void gc_register_mrk(gc_t *gc, void *ptr, gc_mark_t mrk);
+void gc_register_root(gc_t *gc, void *ptr);
+
+size_t gc_get_allocated(gc_t *gc);
+size_t gc_get_reachable(gc_t *gc);
+size_t gc_get_collectable(gc_t *gc);
+
+#define gc_collect_if_needed(gc)                        \
+{                                                       \
+    if (((gc)->flags & GC_COLLECT) &&                   \
+        ((gc)->dirty > GC_MIN_AUTO_COLLECT_SIZE))       \
+        gc_collect(gc);                                 \
+}
+
+#endif
---
+++ src/gc/minim-gc/gc-types.h
@@ -0,0 +1,63 @@
+#ifndef _MINIM_GC_TYPES_H_
+#define _MINIM_GC_TYPES_H_
+
+#include <stdint.h>
+
+/* Block flag */
+#define GC_RECORD_MARK       0x1
+#define GC_RECORD_ROOT       0x2
+
+/* GC flag */
+#define GC_COLLECT          0x1
+
+/* Destructor signature */
+typedef void (*gc_dtor_t)(void*);
+
+/* Marking signature */
+typedef void (*gc_mark_t)(void*,void*,void*);
+
+/* GC bucket type */
+typedef struct gc_record_t {
+    struct gc_record_t *next;
+    size_t size;
+    gc_dtor_t dtor;
+    gc_mark_t mrk;
+} gc_record_t;
+
+/* Main GC type */
+typedef struct gc_t {
+    gc_record_t **buckets;
+    void *stack_bottom;
+    size_t *alloc_ptr;
+    size_t alloc, size;
+    size_t allocs, dirty;
+    uint8_t flags;
+} gc_t;
+
+/* Accessors */
+
+#define gc_record_ptr(r)            ((void *) (((uintptr_t) (r)) + sizeof(gc_record_t)))
+#define gc_record_next(r)           ((gc_record_t *) (((uintptr_t) (r)->next) & ~0x3))
+#define gc_record_flags(r)          (((uintptr_t) (r)->next) & 0x3)
+#define gc_record_alloc_size(r)     ((r)->size + sizeof(gc_record_t))
+
+#define gc_load(gc)     (((double) (gc)->size) / ((double) (gc)->alloc))
+#define gc_hash(p)      (((size_t) (p)) >> 3)
+
+/* Predicates */
+
+#define gc_record_markp(r)      (gc_record_flags(r) & GC_RECORD_MARK)
+#define gc_record_rootp(r)      (gc_record_flags(r) & GC_RECORD_ROOT)
+
+/* Setters */
+
+#define gc_record_mark_set(r)       (r)->next = ((gc_record_t *) (((uintptr_t) (r)->next) | GC_RECORD_MARK));
+#define gc_record_mark_unset(r)     (r)->next = ((gc_record_t *) (((uintptr_t) (r)->next) & ~GC_RECORD_MARK));
+
+#define gc_record_root_set(r)       (r)->next = ((gc_record_t *) (((uintptr_t) (r)->next) | GC_RECORD_ROOT));
+#define gc_record_root_unset(r)     (r)->next = ((gc_record_t *) (((uintptr_t) (r)->next) & ~GC_RECORD_ROOT));
+
+#define gc_record_next_set(r, n)    \
+    ((r)->next = (void *) (((uintptr_t) (n)) | ((uintptr_t) gc_record_flags(r))))
+
+#endif
