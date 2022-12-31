5b842491adbd341c408c278f66b28df23ad31aef
Brett Saiki <bksaiki@gmail.com>
2022-12-30 23:39:25 -0800

    still working (wip)

--- src/gc/gc.h
+++ src/gc/gc.h
@@ -2,7 +2,19 @@
 #define _MINIM_GC_SHIM_H_
 
 #ifdef USE_BOEHM_GC
-#error "unsupported"
+#include "../../boehm-gc/include/gc.h"
+
+#define GC_init(x)             GC_init()          
+#define GC_alloc(n)            GC_malloc(n)
+#define GC_alloc_atomic(n)     GC_malloc_atomic(n)
+#define GC_calloc(s, n)        GC_malloc((s) * (n))
+#define GC_register_root(o)    GC_REG
+
+#define GC_REGISTER_LOCAL_ARRAY(x)
+#define GC_finalize()
+#define GC_pause()
+#define GC_resume()
+
 #else
 #include "minimgc/gc.h"
 #endif
