8c7548962feaf8f3e62b982dc709110b9493a1ac
Brett Saiki <bksaiki@gmail.com>
2023-06-25 18:10:07 -0700

    fix module reader

--- src/core/c/hashtable.c
+++ src/core/c/hashtable.c
@@ -141,9 +141,9 @@ static size_t equal_hash2(minim_object *o, size_t hash) {
     switch (o->type)
     {
     case MINIM_SYMBOL_TYPE:
-        return hash_bytes(&minim_symbol(o), strlen(minim_symbol(o)), hash);
+        return hash_bytes(minim_symbol(o), strlen(minim_symbol(o)), hash);
     case MINIM_STRING_TYPE:
-        return hash_bytes(&minim_string(o), strlen(minim_string(o)), hash);
+        return hash_bytes(minim_string(o), strlen(minim_string(o)), hash);
     case MINIM_PAIR_TYPE:
         return equal_hash2(minim_cdr(o), equal_hash2(minim_car(o), hash));
     case MINIM_VECTOR_TYPE:
