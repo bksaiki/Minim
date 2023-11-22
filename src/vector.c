cf93493d8f1a3130a81fd781908a1b09e08ad083
Brett Saiki <bksaiki@gmail.com>
2023-11-22 00:33:42 -0800

    more prims

--- src/vector.c
+++ src/vector.c
@@ -8,15 +8,15 @@ mobj vector_length(mobj v) {
 
 mobj vector_ref(mobj v, mobj i) {
     mobj len = Mfixnum(minim_vector_len(v));
-    if (minim_negativep(i) || fix_ge(i, len)) 
-        error1("vector_ref", "index is negative", i);
+    if (minim_negativep(i) || minim_truep(fix_ge(i, len)))
+        error1("vector_ref", "index out of bounds", i);
     return minim_vector_ref(v, minim_fixnum(i));
 }
 
 mobj vector_set(mobj v, mobj i, mobj o) {
     mobj len = Mfixnum(minim_vector_len(v));
-    if (minim_negativep(i) || fix_ge(i, len)) 
-        error1("vector_set", "index is negative", i);
+    if (minim_negativep(i) || minim_truep(fix_ge(i, len)))
+        error1("vector_set", "index out of bounds", i);
     minim_vector_ref(v, minim_fixnum(i)) = o;
     return minim_void;
 }
